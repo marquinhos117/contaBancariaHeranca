61890f2e7e867c7219e6ccb1ce378aa5679ae8ab
Marcos Eduardo <102836971+marquinhos117@users.noreply.github.com>
2025-05-14 19:44:15 -0300

    Update ContaEmpresarial.java

--- Conta/ContaEmpresarial.java
+++ Conta/ContaEmpresarial.java
@@ -17,7 +17,7 @@ public class ContaEmpresarial extends Conta {
     }
 
 
-    @Override
+    
     public boolean sacar(double valor){
         if(getSaldo() > 0 && valor <= getSaldo() * 2){
             setSaldo(getSaldo() - valor);
